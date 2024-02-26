98d6411537509ffbd07943a800b0606868a1b6b9
Ibenhoe <iben.vant@gmail.com>
2024-02-26 16:29:00 +0100

    Commit 5
    problems

--- Oef 1/Javascript/javascript.js
+++ Oef 1/Javascript/javascript.js
@@ -1,10 +1,18 @@
+let kleuren = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black'];
 
 
 
 let kleur = document.getElementById('kleur2');
 
 kleur.addEventListener('input', function () {
-    console.log(kleur.value);
+    for (let i = 0; i < kleuren.length; i++) {
+        if (kleur.value == kleuren[i]) {
+            kleur.style.color = kleuren[i];
+        }
+        
+
+    }
+    
 });
 
     
