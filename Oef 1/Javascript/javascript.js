9996e361621460ede0bcfb0cfc97f674469e3821
Ibenhoe <iben.vant@gmail.com>
2024-02-29 16:29:02 +0100

    Commit 6
    Ervoor gezorgd dat men een het formulier kan verzenden

--- Oef 1/Javascript/javascript.js
+++ Oef 1/Javascript/javascript.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 let kleuren = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black'];
 
 
@@ -8,6 +11,7 @@ kleur.addEventListener('input', function () {
     for (let i = 0; i < kleuren.length; i++) {
         if (kleur.value == kleuren[i]) {
             kleur.style.color = kleuren[i];
+            return;
         }
         
 
@@ -15,6 +19,57 @@ kleur.addEventListener('input', function () {
     
 });
 
-    
-    
 
+function validerenForm() {
+    var name = document.getElementById('naam').value;
+    var lastName = document.getElementById('achternaam').value;
+    var email = document.getElementById('email').value;
+    var kleur = document.getElementById('kleur2').value;
+    var checkbox = document.getElementById('box-voorwaarden').checked;
+
+    if (name === '' || lastName === '' || email === '' || kleur === '' || !checkbox) {
+        alert('Vul alle velden in en vink de voorwaarden aan.');
+    } else if (!emailValideren(email)) {
+        alert('Voer een geldig e-mailadres in.');
+    } else {
+        alert('Formulier verzonden!');}
+
+function emailValideren(email) {
+    var reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return reg.test(email);
+        }
+    }
+/*
+
+document.addEventListener('submit', function (e) {
+    var naam = document.getElementById('naam').value;
+    var email = document.getElementById('email').value;
+    var achternaam = document.getElementById('achternaam').value;
+    var kleur = document.getElementById('kleur2').value;
+    var checkbox = document.getElementById('box-voorwaarden').checked;
+
+
+    if (naam == "") {
+        alert("Naam is verplicht");
+        return false;
+    }else if (achternaam == "") {
+        alert("Achternaam is verplicht");
+        return false;
+    } else if (!emailValideren(email)) {
+        alert("Email is verplicht");
+        return false;
+    }else{
+        alert("Formulier is verstuurd");
+    }
+});	
+
+
+
+
+
+function emailValideren(email) {
+    var reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return reg.test(email);
+}
+    
+*/
