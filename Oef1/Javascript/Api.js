062bed871166017481f7cb0a07e2178988d918bf
Ibenhoe <iben.vant@gmail.com>
2024-03-18 16:18:08 +0100

    Commit 23
    Local Data toegevoegd om de prijzen van bitcoin en ethereum bij te houden. Zodat ik hier later nog een grafiek kan laten maken.

--- Oef1/Javascript/Api.js
+++ Oef1/Javascript/Api.js
@@ -10,7 +10,7 @@ const fetchData = async () => {
     });
 
     // Log de data die je ontvangt
-    console.log(response.data);
+    console.log("API data: Successfull");
 
     // Sla de data op in een lokale bestand zonder de tags
     const dataWithoutTags = removeTags(response.data);
@@ -34,53 +34,3 @@ function removeTags(data) {
   // om de tags te verwijderen
   return data;
 }
-
-
-
-
-
-
-/*
-// Maak een asynchrone functie voor het ophalen van de Bitcoin-prijs
-async function fetchBitcoinPrice() {
-  const url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'; // De URL van de API
-
-  try {
-    // Maak het HTTP GET-verzoek met fetch
-    const response = await fetch(url, {
-      headers: {
-        'X-CMC_PRO_API_KEY': '753e978f-0845-4046-8b8f-6a093111ecdc',
-      },
-    });
-
-    // Controleer of het verzoek succesvol was (status 200)
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    // Haal de JSON-gegevens op uit de respons
-    const data = await response.json();
-
-    // Haal de prijs van Bitcoin op uit de JSON-gegevens
-    const bitcoinPrice = data.data['1'].quote.USD.price;
-
-    // Geef de Bitcoin-prijs terug
-    return bitcoinPrice;
-  } catch (error) {
-    // Vang eventuele fouten op en log ze naar de console
-    console.error('Error fetching Bitcoin price:', error);
-    return null;
-  }
-}
-
-// Roep de fetchBitcoinPrice-functie aan en verwerk de geretourneerde gegevens
-fetchBitcoinPrice()
-  .then(price => {
-    // Handel de Bitcoin-prijs verder af
-    console.log('Bitcoin price:', price);
-  })
-  .catch(error => {
-    // Handel eventuele fouten af
-    console.error(error);
-  });
-*/
--- Oef1/Javascript/ReadapiData.js
+++ Oef1/Javascript/ReadapiData.js
@@ -1,8 +1,10 @@
+const { time } = require('console');
 const fs = require('fs');
 const path = require('path');
 
+
 // Import and execute the code from Api.js
-//require('./Api.js');
+require('./Api.js');
 
 // Lees de inhoud van apiData.json
 // Lees de inhoud van het JSON-bestand
@@ -20,20 +22,28 @@ const bitcoinPrice = jsonData.data["1"].quote.USD.price;
 const ethereumObject = jsonData.data["1027"].quote.USD;
 const { price: ethPrice, volume_24h: ethVolume, volume_change_24h: ethVolumeChange, market_cap: ethMarketCap } = ethereumObject;
 
-console.log('Ethereum price:', ethPrice);
-console.log('Ethereum volume 24h:', ethVolume);
-console.log('Ethereum volume change 24h:', ethVolumeChange);
-console.log('Ethereum market cap:', ethMarketCap);
+console.log('Ethereum price:', ethPrice, 'USD');
+console.log('Ethereum volume 24h:', ethVolume, 'USD');
+console.log('Ethereum volume change 24h:', ethVolumeChange, '%');
+console.log('Ethereum market cap:', ethMarketCap, 'USD');
 console.log('----------------------------');
 
 const bitcoinObject = jsonData.data["1"].quote.USD;
 const { price: btcPrice, volume_24h: btcVolume, volume_change_24h: btcVolumeChange, market_cap: btcMarketCap } = bitcoinObject;
-console.log('Bitcoin price:', btcPrice);
-console.log('Bitcoin volume 24h:', btcVolume);
-console.log('Bitcoin volume change 24h:', btcVolumeChange);
-console.log('Bitcoin market cap:', btcMarketCap); 
+console.log('Bitcoin price:', btcPrice, 'USD');
+console.log('Bitcoin volume 24h:', btcVolume, 'USD');
+console.log('Bitcoin volume change 24h:', btcVolumeChange, '%');
+console.log('Bitcoin market cap:', btcMarketCap, 'USD'); 
 console.log('----------------------------');
 
+
+// Schrijf de prijs van Ethereum naar een bestand
+const ethereumPriceFile = path.join(__dirname, 'ethereumPrice.txt');
+fs.writeFileSync(ethereumPriceFile, `Ethereum price: ${ethPrice} USD\n`, { flag: 'a' },Date );
+
+// Schrijf de prijs van Bitcoin naar een bestand
+const bitcoinPriceFile = path.join(__dirname, 'bitcoinPrice.txt');
+fs.writeFileSync(bitcoinPriceFile, `Bitcoin price: ${btcPrice} USD\n`, { flag: 'a' },Date);
 
 // Log de prijs van Ethereum in de console
 //console.log('Ethereum price:', ethereumPrice);
